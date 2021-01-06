07c71bf175e365ca1f0a638b406b129297f9937c
Ludvig Hozman <ludvig123@gmail.com>
2021-01-06 22:46:05 +0100

    Remove dconf settings and add script to set settings instead

---
+++ ubuntu.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# Keyboard repeat interval and delay
+gsettings set org.gnome.desktop.peripherals.keyboard delay 200
+gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 15
