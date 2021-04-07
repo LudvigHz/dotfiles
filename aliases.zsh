4451cf4c8c315e27db3d5e2729a70ae77f01761a
Ludvig Hozman <ludvig123@gmail.com>
2021-04-07 12:32:59 +0200

    Use bat instead of cat when available

--- aliases.zsh
+++ aliases.zsh
@@ -1,7 +1,6 @@
 # General
 alias ..="cd ../"
 
-
 # Git
 alias gd="git diff"
 alias gs="git status"
@@ -11,8 +10,7 @@ alias gcm="git checkout master"
 
 # Programs
 alias gotop="gotop --color=monokai -p -b"
-[ $(command -v ccat) ] && alias cat='ccat -G Keyword="darkgreen" -G Type="darkblue" -G Punctuation="lightgray" -G Plaintext="reset" -G Comment="darkgray"'
-alias ocat="cat"
+[ $(command -v bat) ] && alias cat='bat'
 
 # Open modified files
 # ACMR = Added || Copied || Modified || Renamed
@@ -20,26 +18,19 @@ alias vd="vim \$(git diff HEAD --name-only --diff-filter=ACMR)"
 alias vds="vim \$(git diff --staged --name-only --diff-filter=ACMR)"
 alias vdc="vim \$(git diff HEAD^ --name-only --diff-filter=tm)"
 
-
 # Clipboard
 # $<some command> | copy
 alias copy="xclip -sel clip"
 
 # XDG open
-alias fm="xdg-open ." # Open file manager
-
+alias o=xdg-open
 
 # Asciinema to GIF
 alias asciicast2gif="docker run --rm -v $(pwd):/data asciinema/asciicast2gif"
-
 
 # Ansible
 alias av="ansible-vault"
 alias ap="ansible-playbook"
 
-
 # Sudo vim
 alias svim="sudoedit"
-
-alias o=xdg-open
-
